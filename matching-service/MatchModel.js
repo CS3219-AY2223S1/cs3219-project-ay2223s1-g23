99033d1e57991b2d52ecc0176f34246d76fdaf54
Wayne Tan <waynetan2000@gmail.com>
2022-09-03 13:38:39 +0800

    init sqlite database, init matchmodel, create CRUD routes

---
+++ matching-service/MatchModel.js
@@ -0,0 +1,15 @@
+import { Model, DataTypes } from 'sequelize';
+
+import sequelize from "./database.js";
+
+class MatchModel extends Model { }
+
+MatchModel.init({
+    userId: DataTypes.UUID,
+    difficulty: DataTypes.STRING
+}, {
+    sequelize,
+    modelName: 'matchModel'
+});
+
+export default MatchModel;
