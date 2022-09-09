6bd27b520bd5302070f8c59a7da064387568d940
olivia johansen <55739990+oliviajohansen@users.noreply.github.com>
2022-09-09 10:26:17 +0800

    Matching feature FR2.2-2.5 (#7)
    * add gitignore for nodemodules and .env
    
    * init sqlite database, init matchmodel, create CRUD routes
    
    * add difficultySelection component (radio buttons and submit button)
    
    * add npm uuid for generating userId
    
    * post insert diff
    
    * format typescript
    
    * git ignore dev instance
    
    * allowNull false userId
    
    * install deps
    
    * Set up matching and navigation to empty room
    
    * Prepare room and clean up code
    
    * Set up matching dialog and leave room option
    
    * Clean up
    
    Co-authored-by: Wayne Tan <waynetan2000@gmail.com>

--- frontend/src/components/DifficultySelection.js
+++ frontend/src/components/DifficultySelection.js
@@ -1,33 +1,87 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { URL_INSERT_DIFFICULTY } from "../configs";
 import { STATUS_CODE_CREATED } from "../constants";
 import { v4 as uuidv4 } from "uuid";
+import { useNavigate } from "react-router-dom";
+import MatchingDialog from "./MatchingDialog";
 
-class DifficultySelection extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedDifficulty: "easy",
-      userId: uuidv4(), // random uuid
-    };
-  }
+export const MatchStatus = {
+  NOT_MATCHING: "NOT_MATCHING",
+  MATCHING: "MATCHING",
+  MATCH_SUCCESS: "MATCH_SUCCESS",
+  MATH_FAILED: "MATCH_FAILED",
+};
 
-  handleOptionChange = (event) => {
-    this.setState({
-      selectedDifficulty: event.target.value,
+function DifficultySelection({ socket }) {
+  const [selectedDifficulty, setSelectedDifficulty] = useState(null);
+  const [userId, setUserId] = useState(uuidv4()); // random uuid
+  const [matchStatus, setMatchStatus] = useState(MatchStatus.NOT_MATCHING);
+  const [isMatchingDialogOpen, setIsMatchingDialogOpen] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    socket.on("match_success", (data) => {
+      successFindingMatch();
+      const roomId = data.roomId;
+      navigate(`/room/${roomId}`);
+      socket.emit("join_room", roomId);
+      socket.emit("prep_room", data);
+    });
+    return () => socket.off("match_success");
+  }, [socket]);
+
+  const startFindingMatch = () => {
+    setMatchStatus(MatchStatus.MATCHING);
+    setIsMatchingDialogOpen(true);
+    socket.emit("find_match", {
+      userId: userId,
+      difficulty: selectedDifficulty,
     });
   };
 
-  handleFormSubmit = async (event) => {
-    event.preventDefault();
-    console.log("Your userId: ", this.state.userId);
-    console.log("You have selected: ", this.state.selectedDifficulty);
+  const failedFindingMatch = () => {
+    setMatchStatus(MatchStatus.MATH_FAILED);
+    socket.emit("stop_find_match", {
+      userId: userId,
+      difficulty: selectedDifficulty,
+    });
+  };
 
+  const stopFindingMatch = () => {
+    setIsMatchingDialogOpen(false);
+    if (matchStatus !== MatchStatus.MATH_FAILED) {
+      setMatchStatus(MatchStatus.NOT_MATCHING);
+      socket.emit("stop_find_match", {
+        userId: userId,
+        difficulty: selectedDifficulty,
+      });
+    }
+  };
+
+  const successFindingMatch = () => {
+    setMatchStatus(MatchStatus.MATCH_SUCCESS);
+    setIsMatchingDialogOpen(false);
+  };
+
+  const handleOptionChange = (event) => {
+    setSelectedDifficulty(event.target.value);
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    if (!selectedDifficulty) {
+      console.log("You must select a difficulty level before matching");
+      return;
+    }
+    console.log("Your userId: ", userId);
+    console.log("You have selected: ", selectedDifficulty);
+
+    // Post difficult level chosen.
     const res = await axios
       .post(URL_INSERT_DIFFICULTY, {
-        userId: this.state.userId,
-        difficulty: this.state.selectedDifficulty,
+        userId: userId,
+        difficulty: selectedDifficulty,
       })
       .catch(() => {
         console.log("Please try again later");
@@ -35,56 +89,64 @@ class DifficultySelection extends React.Component {
     if (res && res.status === STATUS_CODE_CREATED) {
       console.log("Difficulty successfully inserted");
     }
+
+    // Start finding match for the user.
+    startFindingMatch();
   };
 
-  render() {
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-12">
-            <form onSubmit={this.handleFormSubmit}>
-              <div className="radio">
-                <label>
-                  <input
-                    type="radio"
-                    value="easy"
-                    checked={this.state.selectedDifficulty === "easy"}
-                    onChange={this.handleOptionChange}
-                  />
-                  Easy
-                </label>
-              </div>
-              <div className="radio">
-                <label>
-                  <input
-                    type="radio"
-                    value="medium"
-                    checked={this.state.selectedDifficulty === "medium"}
-                    onChange={this.handleOptionChange}
-                  />
-                  Medium
-                </label>
-              </div>
-              <div className="radio">
-                <label>
-                  <input
-                    type="radio"
-                    value="hard"
-                    checked={this.state.selectedDifficulty === "hard"}
-                    onChange={this.handleOptionChange}
-                  />
-                  Hard
-                </label>
-              </div>
-              <button className="btn btn-default" type="submit">
-                Match
-              </button>
-            </form>
-          </div>
+  return (
+    <div className="container">
+      <MatchingDialog
+        initSeconds={30}
+        isOpen={isMatchingDialogOpen}
+        handleClose={stopFindingMatch}
+        matchStatus={matchStatus}
+        failedFindingMatch={failedFindingMatch}
+      />
+      <div className="row">
+        <div className="col-sm-12">
+          <form onSubmit={handleFormSubmit}>
+            <div className="radio">
+              <label>
+                <input
+                  type="radio"
+                  value="easy"
+                  checked={selectedDifficulty === "easy"}
+                  onChange={handleOptionChange}
+                />
+                Easy
+              </label>
+            </div>
+            <div className="radio">
+              <label>
+                <input
+                  type="radio"
+                  value="medium"
+                  checked={selectedDifficulty === "medium"}
+                  onChange={handleOptionChange}
+                />
+                Medium
+              </label>
+            </div>
+            <div className="radio">
+              <label>
+                <input
+                  type="radio"
+                  value="hard"
+                  checked={selectedDifficulty === "hard"}
+                  onChange={handleOptionChange}
+                />
+                Hard
+              </label>
+            </div>
+            <button className="btn btn-default" type="submit">
+              Match
+            </button>
+          </form>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default DifficultySelection;
---
+++ frontend/src/components/MatchingDialog.js
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from "react";
+import { Modal, Box, Typography, Button } from "@mui/material";
+import { MatchStatus } from "./DifficultySelection";
+
+function MatchingDialog({
+  initSeconds,
+  isOpen,
+  handleClose,
+  matchStatus,
+  failedFindingMatch,
+}) {
+  const [seconds, setSeconds] = useState(initSeconds);
+  let title = "";
+  let description = "";
+
+  useEffect(() => {
+    if (matchStatus === MatchStatus.MATCHING) {
+      let myInterval = setInterval(() => {
+        if (seconds > 0) {
+          setSeconds(seconds - 1);
+        }
+        if (seconds === 0) {
+          clearInterval(myInterval);
+          failedFindingMatch();
+        }
+      }, 1000);
+      return () => {
+        clearInterval(myInterval);
+      };
+    }
+  });
+
+  useEffect(() => {
+    if (!isOpen) {
+      return;
+    }
+    setSeconds(30);
+  }, [isOpen]);
+
+  switch (matchStatus) {
+    case MatchStatus.MATCHING:
+      title = "Finding a match...";
+      description = "Please wait.";
+      break;
+    case MatchStatus.NOT_MATCHING:
+      title = "Matching cancelled.";
+      description = "";
+      break;
+    case MatchStatus.MATCH_SUCCESS:
+      title = "Found a match!";
+      description = "";
+      break;
+    case MatchStatus.MATH_FAILED:
+      title = "Failed to find a match.";
+      description = "Please try again later.";
+      break;
+    default:
+      console.log("Invalid matchStatus supplied to MatchingDialog");
+  }
+
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
+
+  return (
+    <Modal
+      open={isOpen}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <Typography id="modal-modal-title" variant="h6" component="h2">
+          {title}
+        </Typography>
+        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+          {description} Timer: {seconds} seconds
+        </Typography>
+        <Button variant="outlined" onClick={handleClose}>
+          Cancel
+        </Button>
+      </Box>
+    </Modal>
+  );
+}
+
+export default MatchingDialog;
