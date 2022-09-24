dbc7e0591f843596912bc3327fa68ada8b34eb3a
TianYong-Goh <66197002+TianYong-Goh@users.noreply.github.com>
2022-09-24 23:40:33 +0800

    style reset password page

--- frontend/src/components/user/ResetPasswordPage.js
+++ frontend/src/components/user/ResetPasswordPage.js
@@ -10,6 +10,10 @@ import {
   TextField,
   Grid,
   Paper,
+  Table,
+  TableRow,
+  TableCell,
+  Container,
 } from "@mui/material";
 import { useState } from "react";
 import axios from "axios";
@@ -56,68 +60,69 @@ function ResetPasswordPage() {
 
   return (
     <Grid container>
-      <Grid item xs={1} />
-      <Grid item xs={10}>
-        <Paper elevation={3}>
+      <Grid item xs={2} />
+      <Grid item xs={8} display="flex" justifyContent={"center"}>
+        <Paper elevation={3} sx={{ width: "65%", margin: "auto" }}>
           <Box display={"flex"} alignItems={"center"} justifyContent={"center"}>
-            <Typography variant={"h3"} ma={"2rem"}>
+            <Typography variant={"h3"} m={"1rem"}>
               Reset Password
             </Typography>
           </Box>
+          <Container fixed>
+            <Table aria-label="simple table" sx={{ "& td": { border: 0 } }}>
+              <TableRow>
+                <TableCell sx={{ pl: "6rem" }}>
+                  <Typography variant={"body1"}>Username</Typography>
+                </TableCell>
+                <TableCell sx={{ width: "50%" }}>
+                  <TextField
+                    variant="filled"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    sx={{ marginBottom: "1rem" }}
+                    autoFocus
+                  />
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell sx={{ pl: "6rem" }}>
+                  <Typography variant={"body1"}>New Password</Typography>
+                </TableCell>
+                <TableCell sx={{ width: "50%" }}>
+                  <TextField
+                    variant="filled"
+                    type="email"
+                    value={newPassword}
+                    onChange={(e) => setNewPassword(e.target.value)}
+                    sx={{ marginBottom: "2rem" }}
+                  />
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell sx={{ pl: "6rem" }}>
+                  <Typography variant={"body1"}>Confirm New Password</Typography>
+                </TableCell>
+                <TableCell sx={{ width: "50%" }}>
+                  <TextField
+                    variant="filled"
+                    type="confirm password"
+                    value={confirmNewPassword}
+                    onChange={(e) => setConfirmNewPassword(e.target.value)}
+                    sx={{ marginBottom: "2rem" }}
+                  />
+                </TableCell>
+              </TableRow>
+            </Table>
+          </Container>
           <Box
             display={"flex"}
             flexDirection={"column"}
             alignItems={"center"}
             justifyContent={"center"}>
-            <Box
-              display={"flex"}
-              flexDirection={"row"}
-              alignItems={"center"}
-              justifyContent={"center"}>
-              <Typography>Username</Typography>
-              <TextField
-                variant="filled"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                sx={{ marginBottom: "1rem" }}
-                autoFocus
-              />
-            </Box>
-            <Box
-              display={"flex"}
-              flexDirection={"row"}
-              alignItems={"center"}
-              justifyContent={"center"}>
-              <Typography>New Password</Typography>
-              <TextField
-                variant="filled"
-                type="email"
-                value={newPassword}
-                onChange={(e) => setNewPassword(e.target.value)}
-                sx={{ marginBottom: "2rem" }}
-              />
-            </Box>
-            <Box
-              display={"flex"}
-              flexDirection={"row"}
-              alignItems={"center"}
-              justifyContent={"center"}>
-              <Typography>Confirm New Password</Typography>
-              <TextField
-                variant="filled"
-                type="confirm password"
-                value={confirmNewPassword}
-                onChange={(e) => setConfirmNewPassword(e.target.value)}
-                sx={{ marginBottom: "2rem" }}
-              />
-            </Box>
-          </Box>
-          <Box
-            display={"flex"}
-            flexDirection={"column"}
-            alignItems={"center"}
-            justifyContent={"center"}>
-            <Button variant={"contained"} onClick={handleResetPassword}>
+            <Button
+              variant={"contained"}
+              onClick={handleResetPassword}
+              sx={{ marginBottom: "2rem" }}>
               Reset
             </Button>
           </Box>
@@ -134,7 +139,7 @@ function ResetPasswordPage() {
           </Dialog>
         </Paper>
       </Grid>
-      <Grid item xs={1} />
+      <Grid item xs={2} />
     </Grid>
   );
 }
