9c80eee06ea911ed5d9634bf5355c97484f46be3
TianYong-Goh <66197002+TianYong-Goh@users.noreply.github.com>
2022-09-27 11:43:11 +0800

    logout and route to login

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -12,6 +12,7 @@ import {
 import MenuIcon from "@mui/icons-material/Menu";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
   const [anchorEl, setAnchorEl] = useState(null);
@@ -22,6 +23,19 @@ export default function Navbar() {
     setAnchorEl(null);
   };
   const open = Boolean(anchorEl);
+  function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  const navigate = useNavigate();
+  const handleLogout = async () => {
+    const token = "";
+    setCookie("token", token, 0);
+    console.log(document.cookie);
+    navigate("/login");
+  };
 
   return (
     <AppBar position="static">
@@ -76,7 +90,7 @@ export default function Navbar() {
             <Button variant={"outlined"} sx={{ margin: 1 }} color={"secondary"}>
               Reset Password
             </Button>
-            <Button variant={"contained"} sx={{ margin: 1 }}>
+            <Button variant={"contained"} onClick={handleLogout} sx={{ margin: 1 }}>
               Logout
             </Button>
           </MenuItem>
