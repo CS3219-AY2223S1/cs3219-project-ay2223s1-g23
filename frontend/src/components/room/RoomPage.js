69f82c0f4b55786681ce188497dda2de0236d391
olivia johansen <55739990+oliviajohansen@users.noreply.github.com>
2022-10-01 11:41:55 +0800

    Collaboration service (#23)
    * Set up collaboration service
    
    * Fix collab-service
    
    * Set up collaboration code editor
    
    * Preserve whitespace and add postman collection
    
    * Set up CRUD api for collaboration service
    
    * Add shared code editor
    
    * Resolve merge conflicts
    
    * Resolve merge conflicts

--- frontend/src/components/room/RoomPage.js
+++ frontend/src/components/room/RoomPage.js
@@ -1,51 +1,114 @@
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { IconButton, Paper, Box, Grid, Button, TextField, Typography } from "@mui/material";
 import CallIcon from "@mui/icons-material/Call";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import axios from "axios";
+import { URL_COLLAB } from "../../configs";
+import { STATUS_CODE_BAD_REQUEST } from "../../constants";
+
+const modules = {
+  toolbar: [
+    [{ font: [] }],
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    ["bold", "italic", "underline", "strike"],
+    [{ color: [] }, { background: [] }],
+    [{ script: "sub" }, { script: "super" }],
+    ["blockquote", "code-block"],
+    [{ list: "ordered" }, { list: "bullet" }],
+    [{ indent: "-1" }, { indent: "+1" }, { align: [] }],
+    ["link", "image", "video"],
+    ["clean"],
+  ],
+};
 
 function RoomPage({ socket }) {
+  const { state } = useLocation();
   const [ids, setIds] = useState({
     user1: {
       userId: "",
-      socketId: "",
     },
     user2: {
       userId: "",
-      socketId: "",
     },
   });
-  const [answer, setAnswer] = useState("");
-  // eslint-disable-next-line no-unused-vars
   const [roomId, setRoomId] = useState("");
   const [difficultyLevel, setDifficultyLevel] = useState("");
+  const [value, setValue] = useState("");
   const navigate = useNavigate();
 
   useEffect(() => {
-    socket.on("welcome_room", (data) => {
-      const { userId1, userId2, socketId1, socketId2, roomId, difficulty } = data;
-      setIds({
-        user1: {
-          userId: userId1,
-          socketId: socketId1,
-        },
-        user2: {
-          // user from the queue
-          userId: userId2,
-          socketId: socketId2,
-        },
-      });
-      setRoomId(roomId);
-      setDifficultyLevel(difficulty);
-    });
-    return () => socket.off("welcome_room");
+    fetchRoomDetails();
+  }, []);
+
+  useEffect(() => {
+    const receiveChangesEventHandler = ({ roomId, text }) => {
+      setValue(text);
+    };
+    socket.on("receive-changes", receiveChangesEventHandler);
+    return () => socket.off("receive-changes", receiveChangesEventHandler);
   }, [socket]);
+
+  const fetchRoomDetails = async () => {
+    const res = await axios.get(`${URL_COLLAB}/${state}`).catch((err) => {
+      if (err.response.status === STATUS_CODE_BAD_REQUEST) {
+        console.log("ERROR: " + err.response.data.message);
+      } else {
+        console.log("Please try again later");
+      }
+    });
+    if (res.status != 200) return;
+    const { user1, user2, roomId, difficulty } = res.data.data;
+    setIds({
+      user1: {
+        userId: user1,
+      },
+      user2: {
+        // user from the queue
+        userId: user2,
+      },
+    });
+    setRoomId(roomId);
+    setDifficultyLevel(difficulty);
+  };
+
+  const deleteCollab = async () => {
+    await axios.delete(`${URL_COLLAB}/${roomId}`).catch((err) => {
+      if (err.response.status === STATUS_CODE_BAD_REQUEST) {
+        console.log("ERROR: " + err.response.data.message);
+      } else {
+        console.log("Please try again later");
+      }
+    });
+  };
+
+  const updateCollab = async () => {
+    const res = await axios.put(`${URL_COLLAB}`, { roomId: roomId, text: value }).catch((err) => {
+      if (err.response.status === STATUS_CODE_BAD_REQUEST) {
+        console.log("ERROR: " + err.response.data.message);
+      } else {
+        console.log("Please try again later");
+      }
+    });
+  };
 
   const handleLeaveRoom = () => {
     socket.emit("leave_room", ids.user2.userId);
+    updateCollab();
+    // TODO: add data to history-service
+    deleteCollab();
     navigate(`/diff`);
   };
+
   const handleReset = () => {
-    setAnswer("");
+    socket.emit("send-changes", { roomId: roomId, text: "" });
+  };
+
+  const quillEditorOnChangeHandler = (content, delta, source, editor) => {
+    if (source !== "user") return; // tracking only user changes
+    const text = editor.getText();
+    socket.emit("send-changes", { roomId: roomId, text: text });
   };
 
   return (
@@ -55,12 +118,14 @@ function RoomPage({ socket }) {
           <Box display={"flex"} flexDirection={"row"} mb={"1rem"}>
             <Grid container>
               <Grid item xs={10}>
-                <Typography variant={"h4"}>Topic</Typography>
+                <Typography variant={"h4"}>
+                  {ids.user1.userId} and {ids.user2.userId}&apos;s room
+                </Typography>
               </Grid>
               <Grid item xs={2} display="flex" justifyContent="flex-end">
                 <Paper varient={6}>
                   <Typography variant={"h5"} m={"5px"}>
-                    {difficultyLevel}
+                    {difficultyLevel ?? "unknown diff"}
                   </Typography>
                 </Paper>
               </Grid>
@@ -84,11 +149,13 @@ function RoomPage({ socket }) {
               </Button>
             </Grid>
           </Grid>
-          <TextField
-            multiline
-            value={answer}
-            onChange={(e) => setAnswer(e.target.value)}
-            rows={25}
+          <ReactQuill
+            preserveWhitespace
+            value={value}
+            modules={modules}
+            theme="snow"
+            onChange={quillEditorOnChangeHandler}
+            placeholder="Content goes here..."
           />
           <Box display={"flex"} flexDirection={"row"}>
             <Grid container>
