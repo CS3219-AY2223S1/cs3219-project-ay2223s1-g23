2bcaa8afc145fd261ff1c2a95e98852bdc443658
TianYong-Goh <66197002+TianYong-Goh@users.noreply.github.com>
2022-10-08 19:54:06 +0800

    integrate reset link into navbar

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -9,15 +9,30 @@ import {
   Grid,
   Stack,
   Box,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import DeleteIcon from "@mui/icons-material/Delete";
+import axios from "axios";
+import { URL_USER_SVC_FORGET_PASSWORD } from "../../configs";
 import { useState } from "react";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { STATUS_CODE_BAD_REQUEST, STATUS_CODE_OK } from "../../constants";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   const [anchorEl, setAnchorEl] = useState(null);
   const [open, setOpen] = useState(false);
+  const [isSuccessDialog, setIsSuccessDialog] = useState(false);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [dialogTitle, setDialogTitle] = useState("");
+  const [dialogMsg, setDialogMsg] = useState("");
+  const username = useSelector((state) => state.user.username);
   const navigate = useNavigate();
 
   const handleClick = (event) => {
@@ -39,6 +54,35 @@ export default function Navbar() {
     removeCookie("token");
     navigate("/login");
     window.location.reload();
+  };
+
+  const handleResetPassword = async () => {
+    const res = await axios.post(URL_USER_SVC_FORGET_PASSWORD, { username }).catch((err) => {
+      setIsSuccessDialog(false);
+      if (err.response.status === STATUS_CODE_BAD_REQUEST) {
+        setErrorDialog("ERROR: " + err.response.data.message);
+      } else {
+        setErrorDialog("Please try again later");
+      }
+    });
+    if (res && res.status === STATUS_CODE_OK) {
+      setIsSuccessDialog(true);
+      setSuccessDialog("Password reset link has been sent to your email");
+    }
+  };
+
+  const closeDialog = () => setIsDialogOpen(false);
+
+  const setSuccessDialog = (msg) => {
+    setIsDialogOpen(true);
+    setDialogTitle("Success");
+    setDialogMsg(msg);
+  };
+
+  const setErrorDialog = (msg) => {
+    setIsDialogOpen(true);
+    setDialogTitle("Error");
+    setDialogMsg(msg);
   };
 
   const renderMenu = () => {
@@ -86,9 +130,24 @@ export default function Navbar() {
             </Stack>
           </MenuItem>
           <MenuItem>
-            <Button variant={"outlined"} sx={{ margin: 1 }} color={"secondary"}>
+            <Button
+              variant={"outlined"}
+              onClick={handleResetPassword}
+              color={"secondary"}
+              sx={{ margin: 1 }}>
               Reset Password
             </Button>
+            <Dialog open={isDialogOpen} onClose={closeDialog}>
+              <DialogTitle>{dialogTitle}</DialogTitle>
+              <DialogContent>
+                <DialogContentText>{dialogMsg}</DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button variant={"contained"} onClick={closeDialog} color={"secondary"}>
+                  Done
+                </Button>
+              </DialogActions>
+            </Dialog>
             <Button variant={"contained"} onClick={handleLogout} sx={{ margin: 1 }}>
               Logout
             </Button>
