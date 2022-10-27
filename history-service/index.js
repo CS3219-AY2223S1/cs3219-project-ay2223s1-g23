39220a2a777dcf3c7b8ef06d6bf2d437b0bc0ee5
Wayne Tan <waynetan2000@gmail.com>
2022-10-27 15:00:59 +0800

    APIs for create one history, get one history by id, get histories by userId

--- history-service/index.js
+++ history-service/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { createServer } from "http";
+import { createHistory, getHistoryById, getHistoryByUserId } from "./controllers/history-controller.js";
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +13,11 @@ app.options("*", cors());
 app.get("/", (req, res) => {
     res.send("Hello World from history service");
 });
+app.post("/h", createHistory);
+app.get("/h/id", getHistoryById); // e.g. localhost:8008/h/id?id=1234
+app.get("/h/userId", getHistoryByUserId); // e.g. localhost:8008/h/userId?userId=1234
+
+
 
 const httpServer = createServer(app);
 httpServer.listen(8008, () => {
---
+++ history-service/model/repository.js
@@ -0,0 +1,30 @@
+import 'dotenv/config'
+
+//Set up mongoose connection
+import mongoose from 'mongoose';
+import HistoryModel from './history-model.js';
+
+let mongoDB = process.env.ENV == "PROD" ? process.env.DB_CLOUD_URI : process.env.DB_LOCAL_URI;
+
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+export async function createHistoryModel(params) {
+    return new HistoryModel(params);
+}
+
+export async function getHistoryModelById(id) {
+    return await HistoryModel.findById(id);
+}
+
+export async function getHistoryModelByUserId(userId) {
+    return await HistoryModel.find({
+        _or: [
+            { userId1: userId },
+            { userId2: userId }
+        ]
+    });
+}
