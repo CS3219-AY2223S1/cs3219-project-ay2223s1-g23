d65042a72af7a58147801c34bc5e6fbac7e942a8
TianYong-Goh <66197002+TianYong-Goh@users.noreply.github.com>
2022-11-04 03:48:40 +0800

    update the routing to check caching every access of link

--- frontend/src/util/auth/AuthRoute.js
+++ frontend/src/util/auth/AuthRoute.js
@@ -1,9 +1,20 @@
 import { Outlet, Navigate } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import useAuth from "./useAuth";
+import { getCookie } from "../cookies";
+import jwtDecode from "jwt-decode";
 
 const AuthRoute = () => {
   const auth = useAuth();
+  useEffect(() => {
+    const decodedToken = getCookie("token") ? jwtDecode(getCookie("token")) : {};
+    if (decodedToken && decodedToken.exp * 1000 >= Date.now()) {
+      auth.login();
+    } else {
+      auth.logout();
+    }
+  });
+
   return auth.isLogin ? <Outlet /> : <Navigate to="/login" replace />;
 };
 
