b25725d90c46f590acd9c8c3c94b7e56e630cd55
Wayne Tan <waynetan2000@gmail.com>
2022-11-07 16:49:20 +0800

    add matching api test

--- matching-service/tests/test.js
+++ matching-service/tests/test.js
@@ -10,24 +10,35 @@ chai.should();
 
 const matching_url = "/difficulties";
 var expect = chai.expect;
+const createMatch = (difficulty, done) => {
+    chai.request(app)
+        .post(matching_url)
+        .type("form")
+        .send({
+            userId: "userId1",
+            difficulty: difficulty
+        })
+        .end(async function (err, res) {
+            await MatchModel.findByIdAndDelete(res.body.data._id);
+            res.should.have.status(201);
+            expect(res.body.data.userId).to.be.string("userId1");
+            expect(res.body.data.difficulty).to.be.string(difficulty);
+            expect(res.body.data.matchedUser).to.be.null;
+            done();
+        })
+}
 
 describe("Matching", () => {
     it("POST: create matching with easy", (done) => {
-        chai.request(app)
-            .post(matching_url)
-            .type("form")
-            .send({
-                userId: "userId1",
-                difficulty: "easy"
-            })
-            .end(async function (err, res) {
-                await MatchModel.findByIdAndDelete(res.body.data._id);
-                res.should.have.status(201);
-                expect(res.body.data.userId).to.be.string("userId1");
-                expect(res.body.data.difficulty).to.be.string("easy");
-                expect(res.body.data.matchedUser).to.be.null;
-                done();
-            })
+        createMatch("easy", done);
+    });
+
+    it("POST: create matching with medium", (done) => {
+        createMatch("medium", done);
+    });
+
+    it("POST: create matching with hard", (done) => {
+        createMatch("hard", done);
     });
 
 });
