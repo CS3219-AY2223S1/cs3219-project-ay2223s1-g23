6f5d076976ba2899a46f7a461cc41fd159dfae6f
TianYong-Goh <66197002+TianYong-Goh@users.noreply.github.com>
2022-11-08 02:22:47 +0800

    add delete user test

--- user-service/test/deleteUserTest.js
+++ user-service/test/deleteUserTest.js
@@ -1,32 +1,85 @@
 import { expect } from "chai";
-import { clearAll } from "../model/repository.js";
+import { clearAllUser, getUser } from "../model/repository.js";
 import app from "../index.js";
 import request from "supertest";
+import jwt from "jsonwebtoken";
 import { DELETE_PATH } from "../config.js";
+import userModel from "../model/user-model.js";
 
+const TIMEOUT=4000;
 
-describe("Delete User API Test", () => {
-    beforeEach(async() => {
-        await clearAll();
+describe("Delete User API Test", function() {
+    const fix_username = "test";
+    const fix_email = "test@gmail.com";
+    const fix_password = "test";
+    var token;
+    var res;
+
+    this.timeout(TIMEOUT);
+
+    beforeEach(async () => {
+        await clearAllUser();
+        const newUser = new userModel({username: fix_username, email: fix_email, password: fix_password});
+        await newUser.save();
+        const user = await getUser(fix_username);
+        token = await generateAccessToken(user.toJSON());
     });
 
     afterEach(async () => {
-        await clearAll();
+        await clearAllUser();
     });
 
     describe ("Succcess delete", () => {
         it("valid verification", async () => {
+            res = await deleteUser(fix_username, token);
+            expect(res.status).to.equal(200);
             
+            expect(res.body).to.have.property(
+                "message",
+                "User successfully removed!"
+            );
         });
     });
 
     describe ("Fail delete", () => {
         it("user not exist", async () => {
-        
+            res = await deleteUser("invalidUsername", token); 
+            console.log(res.body);
+            expect(res.status).to.equal(404);
+            expect(res.body).to.have.property(
+                "message",
+                "User not found!"
+            );
+        });
+
+        it("no authentication", async () => {
+            res = await request(app).delete("/api/user/" + DELETE_PATH.replace(":username", fix_username));
+
+            expect(res.status).to.equal(401);
+            expect(res.body).to.have.property(
+                "message",
+                "A token is required for authentication!"
+            );
         });
     
-        it("invalid varification", async () => {
-            
+        it("invalid authentication", async () => {
+            res = await deleteUser(fix_username, "invalidToken"); 
+
+            expect(res.status).to.equal(401);
+            expect(res.body).to.have.property(
+                "message",
+                "Invalid token provided!"
+            );
         });
     });
 });
+
+async function deleteUser(username, token) {
+    return await request(app)
+        .delete("/api/user/" + DELETE_PATH.replace(":username", username))
+        .set({Authorization: "Bearer " + token});
+};
+
+async function generateAccessToken(user) {
+    return jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "1800s" });
+}
